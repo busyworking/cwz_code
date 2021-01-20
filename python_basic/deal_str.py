0fb18c208d1001e3ec0c927a2b68bedd98b05d3f
陈文治 <chenwenzhi@sinorail.com>
2021-01-20 10:36:07 +0800

    add deploy system

---
+++ python_basic/deal_str.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-#
+# Author: chenwenzhi
+# Date: 2020/12/20
+
+
+name_bob = "bob name"
+name = "siliy"
+# print name.title()
+# print name.lower()
+# print name.upper()
+print "\n\ta\n\tb\n\tc"
+
